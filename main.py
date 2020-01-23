54bbec6f1aa6974eef6bdfe0cf47b98f049d4e1f
Sander Meinderts <sander_meinderts@hotmail.com>
2020-01-23 11:08:06 +0100

    Argparser

--- main.py
+++ main.py
@@ -12,30 +12,39 @@ from catan.Players import opponents
 
 from utils import *
 
-args = dotdict({
-    'numIters': 100,
-    'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
-    'tempThreshold': 15,        #
-    'updateThreshold': 0.5,     # During arena playoff, new nnet will be accepted if threshold or more games are won.
-    'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
-    'numMCTSSims': 25,          # Number of games moves for MCTS to simulate.
-    'arenaCompare': 40,         # Number of games to play during arena play to determine if new net will be accepted.
-    'cpuct': 0.5,
+import argparse
 
-    'checkpoint': './temp/',
-    'load_model': False,
-    'load_folder_file': ('./catan_othello_net','checkpoint_9.pth.tar'),
-    'numItersForTrainExamplesHistory': 10,
-    'render': True,
-})
+def create_parser():
+    """Creates a parser for command-line arguments.
+    """
+    parser = argparse.ArgumentParser()
+
+    # Training hyper-parameters
+    parser.add_argument('--numEps', type=int, default=100, help='The number of training epochs to run (you can Ctrl-C out earlier if you want).')
+    parser.add_argument('--numIters', type=int, default=100, help='The number of games in an epoch.')
+    parser.add_argument('--tempThreshold', type=int, default=15)
+    parser.add_argument('--updateThreshold', type=float, default=0.5)
+    parser.add_argument('--maxlenOfQueue', type=int, default=200000)
+    parser.add_argument('--numMCTSSims', type=int, default=25)
+    parser.add_argument('--arenaCompare', type=int, default=40)
+    parser.add_argument('--cpuct', type=float, default=0.5)
+    parser.add_argument('--checkpoint', type=str, default='./temp/')
+    parser.add_argument('--render', type=bool, default=False)
+    parser.add_argument('--load_model', type=bool, default=False)
+    parser.add_argument('--load_folder_file', default=('./catan_othello_net','checkpoint_9.pth.tar'))
+    parser.add_argument('--numItersForTrainExamplesHistory', type=int, default=10)
+
+    return parser
 
 if __name__ == "__main__":
+    parser = create_parser()
+    args = parser.parse_args()
+
     g = Game()
     nnet = nn(g)
 
     if args.load_model:
         nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
-
     c = Coach(g, nnet, args, opponents=opponents)
     if args.load_model:
         print("Load trainExamples from file")
